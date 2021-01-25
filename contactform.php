2672c4476355d2d38fcded30010918902ccc8536
iwasatat0107 <it.was07@gmail.com>
2021-01-25 10:39:29 +0900

    確認画面の作成

--- contactform.php
+++ contactform.php
@@ -6,8 +6,15 @@
 </head>
 <body>
   <?php if( $_POST ){ ?>
-    
-
+    <!-- 確認画面 -->
+    <form action="./contactform.php" method="post">
+      名前    <?php echo $_POST['fullname'] ?><br>
+      Eメール  <?php echo $_POST['email'] ?></br>
+      お問い合わせ内容</br>
+      <?php echo nl2br($_POST['message']) ?><br>
+      <input type="submit" name="back" value="戻る" />
+      <input type="submit" name="send" value="送信" />
+    </form>
   <?php } else { ?>
     <!-- 入力画面 -->
     <form action="./contactform.php" method="post">
@@ -15,7 +22,7 @@
       Eメール<input type="email" name＝"email" value=""></br>
       お問い合わせ内容</br>
       <textarea cols="40" rows="8" name="message"></textarea>
-      <input type="submit" name="confirm" value="確認">
+      <input type="submit" name="confirm" value="確認" />
     </form>
   <?php } ?>
 </body>
